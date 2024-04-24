25005999db753cca6fd579c53553b580856634d1
Mayur <99334268+devopsmayur@users.noreply.github.com>
2024-04-24 11:01:19 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -13,6 +13,6 @@ resource "google_compute_network" "vpc_network" {
 resource "google_compute_subnetwork" "web_subnet" {
   name          = "web-subnet"
   region        = "us-central1"
-  network       = data.google_compute_network.vpc_network.self_link
+  network       = google_compute_network.vpc_network.self_link
   ip_cidr_range = "10.0.1.0/24"
 }
