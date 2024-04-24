638d16dc88774ebe4f5980795cc8745ab38268c1
Mayur <99334268+devopsmayur@users.noreply.github.com>
2024-04-24 11:02:32 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -8,11 +8,3 @@ provider "google" {
 resource "google_compute_network" "vpc_network" {
   name = "my-vpc-network"
 }
-
-# Create subnets
-resource "google_compute_subnetwork" "web_subnet" {
-  name          = "web-subnet"
-  region        = "us-central1"
-  network       = google_compute_network.vpc_network.self_link
-  ip_cidr_range = "10.0.1.0/24"
-}
